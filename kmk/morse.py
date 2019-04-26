5f39e09f05f48ad12b4b4d0c49f5f5f7d0c5e397
Elvis Pfützenreuter <epxx@epxx.co>
2019-04-26 19:37:53 -0300

    Morse class moved to module

---
+++ kmk/morse.py
@@ -0,0 +1,110 @@
+from kmk.keys import KC
+
+morse_codes = {".-": KC.A, "-.": KC.N}
+
+class Morse:
+    def __init__(self):
+        self.code = ""
+        self.state = "OFF"
+        self.pending_timeout = None
+
+    def trans(self, new_state, kbd_state, arg):
+        transition = self.state + "_" +	new_state
+        # if kbd_state.config.debug_enabled:
+        print("transition " + transition)
+        m = getattr(self, transition, None)
+        if not m:
+            # if kbd_state.config.debug_enabled:
+            print("        	Invalid transition")
+            return
+        self.state = new_state
+        m(kbd_state, arg)
+
+    def OFF_STARTING(self, kbd_state, arg):
+        self.trans("ON", kbd_state, arg)
+
+    def STARTING_ON(self, kbd_state, arg):
+        self.code = ""
+        self.pending_timeout = None
+
+    def ON_OFF(self, kbd_state, arg):
+        pass
+
+    def OFF_OFF(self, kbd_state, arg):
+        pass
+
+    def TYPING_OFF(self, kbd_state, arg):
+        self.stop_typing_timeout(kbd_state)
+        pass
+
+    def ON_ON(self, kbd_state, arg):
+        kbd_state.tap_key(KC.SPC)
+
+    def ON_TYPING(self, kbd_state, c):
+        self.code += c
+	if len(self.code) > 10:
+	    self.code = "f"
+        self.start_typing_timeout(kbd_state)
+
+    def TYPING_TYPING(self, kbd_state, c):
+        self.code += c
+	if len(self.code) > 10:
+	    self.code = "f"
+        self.start_typing_timeout(kbd_state)
+
+    def TYPING_ON(self, kbd_state, arg):
+        self.stop_typing_timeout(kbd_state)
+        if self.code not in morse_codes:
+            # if kbd_state.config.debug_enabled:
+            print("Invalid code " + self.code)
+            self.code = ""
+            return
+        key = morse_codes[self.code]
+        self.code = ""
+        kbd_state.tap_key(key)
+
+    def TYPING_TIMEOUT(self, kbd_state, arg):
+        self.code = ""
+        self.trans("ON", kbd_state, arg)
+
+    def TIMEOUT_ON(self, kbd_state, arg):
+        pass
+
+    def TYPING_CANCEL(self, kbd_state, arg):
+        self.stop_typing_timeout(kbd_state)
+        self.code = ""
+        self.trans("ON", kbd_state, arg)
+
+    def CANCEL_ON(self, kbd_state, arg):
+        pass
+
+    def start_typing_timeout(self, kbd_state):
+        self.stop_typing_timeout(kbd_state)
+        self.pending_timeout = kbd_state.set_timeout(2000, self.timeout)
+
+    def stop_typing_timeout(self, kbd_state):
+        if self.pending_timeout is not None:
+            kbd_state.cancel_timeout(self.pending_timeout)
+        self.pending_timeout = None
+   
+    def turn_on(self, kbd_state):
+       self.trans("STARTING", kbd_state, None)
+ 
+    def turn_off(self, kbd_state):
+       self.trans("OFF", kbd_state, None)
+
+    def dot(self, kbd_state):
+        self.trans("TYPING", kbd_state, ".")
+
+    def dash(self, kbd_state):
+        self.trans("TYPING", kbd_state, "-")
+        
+    def spc(self, kbd_state):
+        self.trans("ON", kbd_state, None)
+
+    def cancel(self, kbd_state):
+        self.trans("CANCEL", kbd_state, None)
+
+    def timeout(self):
+        self.pending_timeout = None
+        self.trans("TIMEOUT", None, None)
--- user_keymaps/epx/4x5.py
+++ user_keymaps/epx/4x5.py
@@ -1,7 +1,8 @@
-from kmk.consts import DiodeOrientation,
+from kmk.consts import DiodeOrientation
 from kmk.keys import KC
 from kmk.mcus.circuitpython_samd51 import Firmware
 from kmk.pins import Pin as P
+from kmk.morse import Morse
 
 import adafruit_dotstar
 import board
@@ -10,113 +11,10 @@ led[0] = (255, 255, 255)
 
 keyboard = Firmware()
 
-keyboard.debug_enabled = True
+keyboard.debug_enabled = False
 keyboard.col_pins = (P.D13, P.A3, P.A4, P.A2)
 keyboard.row_pins = (P.D7, P.D10, P.D9, P.D12, P.D11)
 keyboard.diode_orientation = DiodeOrientation.ROWS # ROW2COL
-
-# ------------- Morse handling ----------------------
-
-morse_codes = {".-": KC.A, "-.": KC.N}
-
-class Morse:
-    def __init__(self):
-        self.code = ""
-        self.state = "OFF"
-        self.pending_timeout = None
-
-    def trans(self, new_state, kbd_state, arg):
-        transition = self.state + "_" +	new_state
-        if keyboard.debug_enabled:
-            print("transition " + transition)
-        m = getattr(self, transition, None)
-        if not m:
-            if keyboard.debug_enabled:
-                print("        	Invalid transition")
-            return
-        self.state = new_state
-        m(self, kbd_state, arg)
-
-    def OFF_STARTING(self, kbd_state, arg):
-        self.trans("ON", kbd_state, arg)
-
-    def STARTING_ON(self, kbd_state, arg):
-        self.code = ""
-        self.pending_timeout = None
-
-    def ON_OFF(self, kbd_state, arg):
-        pass
-
-    def TYPING_OFF(self, kbd_state, arg):
-        pass
-
-    def ON_ON(self, kbd_state, arg):
-        kbd_state.tap_key(KC.SPC)
-
-    def ON_TYPING(self, kbd_state, c):
-        self.code += c
-        self.start_typing_timeout(kbd_state)
-
-    def TYPING_TYPING(self, kbd_state, c):
-        self.code += c
-        self.start_typing_timeout(kbd_state)
-
-    def TYPING_ON(self, kbd_state, arg):
-        self.stop_typing_timeout(kbd_state)
-        if self.code not in morse_codes:
-            if keyboard.debug_enabled:
-                print("Invalid code " + self.code)
-            self.code = ""
-            return
-        key = morse_codes[self.code]
-        self.code = ""
-        kbd_state.tap_key(key)
-
-    def TYPING_TIMEOUT(self, kbd_state, arg):
-        self.code = ""
-        self.trans("ON", None)
-
-    def TIMEOUT_ON(self, kbd_state, arg):
-        pass
-
-    def TYPING_CANCEL(self, kbd_state, arg):
-        self.stop_typing_timeout(kbd_state)
-        self.code = ""
-        self.trans("ON", None)
-
-    def CANCEL_ON(self, kbd_state, arg):
-        pass
-
-    def start_typing_timeout(self, kbd_state):
-        self.stop_typing_timeout(kbd_state)
-        self.pending_timeout = kbd_state.set_timeout(2000, self.timeout)
-
-    def stop_typing_timeout(self, kbd_state):
-        if self.pending_timeout is not None:
-            kbd_state.cancel_timeout(self.pending_timeout)
-        self.pending_timeout = None
-   
-    def turn_on(self, kbd_state):
-       self.trans("STARTING", kbd_state)
- 
-    def turn_off(self, kbd_state):
-       self.trans("OFF", kbd_state)
-
-    def dot(self, kbd_state):
-        self.trans("TYPING", kbd_state, ".")
-
-    def dash(self, kbd_state):
-        self.trans("TYPING", kbd_state, "-")
-        
-    def spc(self, kbd_state):
-        self.trans("ON", kbd_state)
-
-    def cancel(self, kbd_state):
-        self.trans("CANCEL", None)
-
-    def timeout(self):
-        self.pending_timeout = None
-        self.trans("TIMEOUT", None)
 
 morse = Morse()
 
@@ -198,6 +96,6 @@ keyboard.keymap = [
 ]
 
 if __name__ == '__main__':
-    led[0] = (255, 255, 0)
+    led[0] = (255, 128, 128)
     keyboard.go()
 
