97e93cb260508548811973be495c8f5dfac71b8b
Elvis Pfutzenreuter <epxx@epxx.co>
2019-06-06 00:08:00 -0300

    Convert ident to 4spaces

--- kmk/ps2matrix.py
+++ kmk/ps2matrix.py
@@ -8,26 +8,26 @@ from kmk.ps2protocol import PS2Protocol
 # all pressed keys to generate release reports for every one).
 
 class PS2MatrixScanner:
-	def __init__(self, cols, *args, **kwargs):
-		self.k = PS2Protocol(cols[0], cols[1])
-		self.state = bytearray(32)
-		self.report = bytearray(3)
+    def __init__(self, cols, *args, **kwargs):
+        self.k = PS2Protocol(cols[0], cols[1])
+        self.state = bytearray(32)
+        self.report = bytearray(3)
 
-	def scan_for_changes(self):
-		matrix = self.k.poll()
-		for row in range(0, 32):
-			if matrix[row] == self.state[row]:
-				continue
-			self.report[0] = row
-			for col in range(0, 8):
-				bit = 1 << col
-				if (matrix[row] & bit) == (self.state[row] & bit):
-					continue
-				self.report[1] = col
-				if matrix[row] & bit: # press
-					self.state[row] |= bit
-					self.report[2] = True
-				else: # release
-					self.state[row] &= ~bit
-					self.report[2] = False
-				return self.report
+    def scan_for_changes(self):
+        matrix = self.k.poll()
+        for row in range(0, 32):
+            if matrix[row] == self.state[row]:
+                continue
+            self.report[0] = row
+            for col in range(0, 8):
+                bit = 1 << col
+                if (matrix[row] & bit) == (self.state[row] & bit):
+                    continue
+                self.report[1] = col
+                if matrix[row] & bit: # press
+                    self.state[row] |= bit
+                    self.report[2] = True
+                else: # release
+                    self.state[row] &= ~bit
+                    self.report[2] = False
+                return self.report
